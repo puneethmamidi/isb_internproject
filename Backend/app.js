7de9d78cfd47905b4571a74a11f9ac8644222859
puneethmamidi <puneethmamidi645@gmail.com>
2024-06-20 17:54:45 +0530

    backend-commit

--- Backend/app.js
+++ Backend/app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors =  require('cors');
+
 const nodemailer = require('nodemailer');
 const bodyParser = require('body-parser');
 const  jwt = require('jsonwebtoken');
--- Backend/database.js
+++ Backend/database.js
@@ -1,10 +1,14 @@
+require('dotenv').config();
 const mysql = require('mysql');
-
+const dbHost = process.env.DB_HOST;
+const dbUser = process.env.DB_USER;
+const dbPassword = process.env.DB_PASSWORD;
+const dbName = process.env.DB_NAME;
 const db = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password:"root123",
-    database:"exp"
+    host: dbHost,
+    user: dbUser,
+    password:dbPassword,
+    database:dbName
  })
 
 
