0ca0ca015f113cf9974ec5fc048430c460b7aad0
puneethmamidi <puneethmamidi645@gmail.com>
2024-06-22 19:32:32 +0530

    backend commit

--- Backend/database.js
+++ Backend/database.js
@@ -4,9 +4,9 @@ const dbHost = process.env.DB_HOST;
 const dbUser = process.env.DB_USER;
 const dbPassword = process.env.DB_PASSWORD;
 const dbName = process.env.DB_NAME;
-console.log(dbName);
 
-const db = mysql.createConnection({
+
+const db = mysql.createPool({
     host: dbHost,
     user: dbUser,
     password: dbPassword,
