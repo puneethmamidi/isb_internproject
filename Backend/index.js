5d63b389f866e0a5e5c79c5b547291c51e9fc864
puneethmamidi <puneethmamidi645@gmail.com>
2024-06-23 23:51:04 +0530

    backend commit

--- Backend/index.js
+++ Backend/index.js
@@ -105,7 +105,7 @@ app.get('/object/:id', (req, res) => {
     res.status(400).json({ error: 'Invalid ID parameter' });
     return;
   }
-  const useDbSql = `USE ${dbName}`;
+  const useDbSql = "USE dbName";
   const sql = "SELECT * FROM words WHERE id = ?";
   
   db.query(useDbSql,sql [id], (err, rows) => {
